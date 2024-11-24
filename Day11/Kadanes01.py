cd8978ccb8a7bbd867c14e5173e2c01f8a18e3d2
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2024-11-24 19:55:10 +0530

    Update Kadanes01.py

--- Day11/Kadanes01.py
+++ Day11/Kadanes01.py
@@ -1,1 +1,38 @@
 
+class Solution:
+    ##Complete this function
+    #Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self,arr):
+        ##Your code here
+        mx = current = arr[0]
+        for i in range(1,len(arr)):
+            current = max(arr[i],current+arr[i])
+            if mx < current:
+                mx = current
+        return mx        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+
+        ob = Solution()
+
+        print(ob.maxSubArraySum(arr))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
